4d53ccd1063ff80e0740fa24bb640fce750d062b
Zaimutiz <zaimutis@gmail.com>
2024-09-19 18:46:41 +0300

    Add files via upload

---
+++ Lab1Synced/Lab1Synced/Program.cs
@@ -0,0 +1,64 @@
+﻿//Real Life Situation could be any Inventory Management System 
+//The is no problem here because when one of the functions take the stock parameter it locks it and the other thread
+//cannot use it untill it is changed/released.
+using System;
+using System.Threading;
+
+class Inventory
+{
+    private int stock = 100;
+    private readonly object stockLock = new object();
+    //Below there is 2 critical sections by adding and removing stock.
+    public void AddStock(int quantity)
+    {
+        lock (stockLock)
+        {
+            Thread.Sleep(10);
+            stock += quantity;  
+        }
+    }
+    public void RemoveStock(int quantity)
+    {
+        lock (stockLock)
+        {
+            Thread.Sleep(10);
+            stock -= quantity;
+        }
+    }
+    public int GetStock()
+    {
+        lock (stockLock)
+        {
+            return stock;
+        }
+    }
+}
+public class SynchronizedInventory
+{
+    public static void Main(string[] args)
+    {
+        for (int run = 1; run <= 5; run++)  
+        {
+            Inventory inventory = new Inventory();
+            Thread addThread = new Thread(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    inventory.AddStock(10);
+                }
+            });
+            Thread removeThread = new Thread(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    inventory.RemoveStock(10);
+                }
+            });
+            addThread.Start();
+            removeThread.Start();
+            addThread.Join();
+            removeThread.Join();
+            Console.WriteLine("Final stock: " + inventory.GetStock());
+        }
+    }
+}
---
+++ Lab1unsync/Lab1unsync/Program.cs
@@ -0,0 +1,54 @@
+﻿//Real Life Situation could be any Inventory Management System
+//The problem is that the stock parameter is not locked and can be used at the same time giving the same first input
+//but different output if the threads use it at the same time
+using System;
+using System.Threading;
+
+class Inventory
+{
+    private int stock = 100; 
+    //Below there is 2 critical sections by adding and removing stock.
+    public void AddStock(int quantity)
+    {
+        Thread.Sleep(10);
+        stock += quantity;
+    }
+    public void RemoveStock(int quantity)
+    {
+        Thread.Sleep(10);
+        stock -= quantity;
+    }
+    public int GetStock()
+    {
+        return stock;
+    }
+}
+public class InventoryRaceCondition
+{
+    public static void Main(string[] args)
+    {
+        for (int run = 1; run <= 5; run++)
+        {
+            Inventory inventory = new Inventory();
+            Thread addThread = new Thread(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    inventory.AddStock(10);
+                }
+            });
+            Thread removeThread = new Thread(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    inventory.RemoveStock(10);
+                }
+            });
+            addThread.Start();
+            removeThread.Start();
+            addThread.Join();
+            removeThread.Join();
+            Console.WriteLine("Final stock: " + inventory.GetStock());
+        }
+    }
+}
